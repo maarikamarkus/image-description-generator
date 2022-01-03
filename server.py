7b0232e3bf588c3646ebb9ec426983fb0ae51dcf
maarikam <maarika.markus@nortal.com>
2022-01-03 21:32:48 +0200

    Use template rendering. Extract some parts of code into separate methods for better readability. Remove commented out code.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
 import image_description_generator
@@ -10,46 +10,53 @@ ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def get_img_desc(request):
+  if 'filename' not in request.files:
+    return render_template('error.html', reason="No file added.")
+
+  file = request.files['filename']
+
+  if file.filename == '':
+    return render_template('error.html', reason="No selected file.")
+
+  if not file or not allowed_file(file.filename):
+    return render_template('error.html', reason="This file type is not supported.")
+
+  filename = secure_filename(file.filename)
+  
+  try:
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(path)
+
+    desc = generate_description_from_file(path)
+
+  finally:
+    remove_file_from_server(path)
+  
+  return desc
+
 def allowed_file(filename):
   return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def generate_description_from_file(path):
+  with open(path, 'rb') as f:
+    img_bytes = f.read()
+    decoded = base64.b64encode(img_bytes).decode('ascii')
+
+  desc = image_description_generator.get_img_desc(path)
+
+  return render_template('result.html', decoded=decoded, desc=desc)
+
+def remove_file_from_server(path):
+  if os.path.exists(path):
+      os.remove(path)
+  return
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
   if request.method == 'GET':
-    with open('index.html') as f:
-      return f.read()
+    return render_template('index.html')
   
   if request.method == 'POST':
-    if 'filename' not in request.files:
-      return 'No file part'
-
-    file = request.files['filename']
-    if file.filename == '':
-      return 'No selected file'
-
-    if not file or not allowed_file(file.filename):
-      return 'bad boy'
-
-    filename = secure_filename(file.filename)
-    
-    try:
-      path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-      file.save(path)
-
-      with open(path, 'rb') as f:
-        img_bytes = f.read()
-        decoded = base64.b64encode(img_bytes).decode('ascii')
-
-      desc = image_description_generator.get_img_desc(path)
-      #desc = "jou"
-
-      return f'''
-          <img src="data:image/jpeg;base64, {decoded}" width=500 style="display: block">
-          <p style="width: 500px">{desc}</p>
-          <a href="/">Wanna try again? ✌️</a>
-        '''
-
-    finally:
-      if os.path.exists(path):
-        os.remove(path)
+    return get_img_desc(request)
